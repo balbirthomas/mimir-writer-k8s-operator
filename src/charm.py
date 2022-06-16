48023c460e67f61c14102d36f31bd077a06a2871
Balbir Thomas <balbir.thomas@canonical.com>
2022-06-16 14:51:12 +0100

    Implemented pebble ready handler
    This commit implements the Mimir writer pebble ready handler,
    which starts the Mimir workload in response to this event. In
    order to start the workload a configuration file is also generated.

--- src/charm.py
+++ src/charm.py
@@ -9,10 +9,24 @@ This charm deploys the write pathway of Mimir.
 """
 
 import logging
+import socket
+import yaml
 
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
+
+from mimir_writer.config import (
+    MIMIR_CONFIG_FILE,
+    MIMIR_DIRS,
+    block_storage_config,
+    compactor_config,
+    distributor_config,
+    ingester_config,
+    memberlist_config,
+    server_config,
+    store_gateway_config,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +36,149 @@ class MimirWriterCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._name = "mimir-writer"
+        self._peername = "mimir-writer-peers"
+
+        # charm lifecycle event handlers
+        self.framework.observe(self.on.mimir_writer_pebble_ready, self._on_mimir_writer_pebble_ready)
+
+    def _on_mimir_writer_pebble_ready(self, event):
+        """Define and start a workload using the Pebble API.
+
+        When a new Mimir writer workload container starts the,
+        all required Mimir directories are created, a new Mimir
+        config file is created and then the Mimir workload is
+        started.
+        """
+        self._create_mimir_dirs()
+        self._set_mimir_config()
+
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Define an initial Pebble layer configuration
+        targets = "distributor,ingester,compactor,store-gateway,purger"
+        pebble_layer = {
+            "summary": "mimir layer",
+            "description": "pebble config layer for mimir",
+            "services": {
+                self._name: {
+                    "override": "replace",
+                    "summary": self._name,
+                    "command": f"mimir -target={targets} --config.file {MIMIR_CONFIG_FILE}",
+                    "startup": "enabled",
+                }
+            },
+        }
+        # Add initial Pebble config layer using the Pebble API
+        container.add_layer(self._name, pebble_layer, combine=True)
+        container.start(self._name)
         self.unit.status = ActiveStatus()
+
+    def _restart_mimir(self):
+        """Restart Mimir workload."""
+        container = self.unit.get_container(self._name)
+
+        if not container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for Pebble ready")
+            return False
+
+        container.stop(self._name)
+        container.start(self._name)
+        self.unit.status = ActiveStatus()
+
+        return True
+
+    def _set_mimir_config(self):
+        """Generate and set a Mimir workload configuration."""
+        container = self.unit.get_container(self._name)
+
+        if not container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for Pebble ready")
+            return False
+
+        # push mimr config file to workload
+        mimir_config = self._mimir_config()
+        container.push(MIMIR_CONFIG_FILE, mimir_config, make_dirs=True)
+        logger.info("Set new Mimir configuration")
+
+        return True
+
+    def _create_mimir_dirs(self):
+        """Create Mimir directories.
+
+        The Mimir writer workload requires many directories to be
+        present before it is started. These directories are
+        used for storing Mimir configuration and metrics blocks
+        locally.
+        """
+        container = self.unit.get_container(self._name)
+
+        if not container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for Pebble ready")
+            return False
+
+        for _, path in MIMIR_DIRS.items():
+            if not container.exists(path):
+                container.make_dir(path, make_parents=True)
+
+        return True
+
+    def _mimir_config(self) -> str:
+        """Generate a Mimir workload configuration."""
+        s3_config = yaml.safe_load(self.config.get("s3", "{}"))
+        retention_period = self.config.get("tsdb_block_retention_period", "24h")
+
+        config = {
+            "multitenancy_enabled": False,
+            "blocks_storage": block_storage_config(s3_config, retention_period),
+            "compactor": compactor_config(),
+            "distributor": distributor_config(),
+            "ingester": ingester_config(),
+            "server": server_config(),
+            "store_gateway": store_gateway_config(),
+            "memberlist": memberlist_config(self.unit.name, self.peers),
+        }
+
+        return yaml.dump(config)
+
+    @property
+    def hostname(self):
+        """Fully qualified hostname of this unit.
+
+        Returns:
+            A string given fully qualified hostname of this unit.
+        """
+        return socket.getfqdn()
+
+    @property
+    def peer_relation(self):
+        """Fetch the peer relation.
+
+        Returns:
+             A :class:`ops.model.Relation` object representing
+             the peer relation.
+        """
+        return self.model.get_relation(self._peername)
+
+    @property
+    def peers(self):
+        """Fetch all peer names and hostnames.
+
+        Returns:
+            A mapping from peer unit names to peer hostnames.
+        """
+        peers = {}
+        peers[self.unit.name] = str(self.hostname)
+
+        if not self.peer_relation:
+            # just return self unit if no peers
+            return peers
+
+        for unit in self.peer_relation.units:
+            if hostname := self.peer_relation.data[unit].get("peer_hostname"):
+                peers[unit.name] = hostname
+
+        return peers
 
 
 if __name__ == "__main__":
---
+++ src/mimir_writer/config.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities to construct Mimir configuration."""
+
+MIMIR_PORT = 9009
+MIMIR_PUSH_PATH = "/api/v1/push"
+MIMIR_CONFIG_FILE = "/etc/mimir/config.yaml"
+
+MIMIR_DIRS = {
+    "bucket_store": "/tmp/mimir/tsdb-sync",
+    "data": "/tmp/mimir/data/tsdb",
+    "tsdb": "/tmp/mimir/tsdb",
+    "compactor": "/tmp/mimir/compactor",
+}
+
+
+def block_storage_config(s3_config, retention_period):
+    """Mimir Blocks Storage configuration."""
+    cfg = {
+        "bucket_store": {"sync_dir": MIMIR_DIRS["bucket_store"]},
+        "tsdb": {"dir": MIMIR_DIRS["tsdb"], "retention_period": retention_period},
+    }
+
+    if s3_config:
+        cfg["backend"] = "s3"
+        cfg["s3"] = s3_config
+    else:
+        cfg["backend"] = "filesystem"
+        cfg["filesystem"] = {"dir": MIMIR_DIRS["data"]}
+
+    return cfg
+
+
+def compactor_config():
+    """Mimir Compactor configuration."""
+    cfg = {
+        "data_dir": MIMIR_DIRS["compactor"],
+        "sharding_ring": {"kvstore": {"store": "memberlist"}},
+    }
+
+    return cfg
+
+
+def distributor_config():
+    """Mimir Distributor configuration."""
+    cfg = {"ring": {"instance_addr": "127.0.0.1", "kvstore": {"store": "memberlist"}}}
+
+    return cfg
+
+
+def ingester_config():
+    """Mimir Ingestor configuration."""
+    cfg = {
+        "ring": {
+            "instance_addr": "127.0.0.1",
+            "kvstore": {
+                "store": "memberlist",
+            },
+            "replication_factor": 1,
+        }
+    }
+
+    return cfg
+
+
+def server_config():
+    """Mimir Server configuration."""
+    cfg = {"http_listen_port": MIMIR_PORT, "log_level": "error"}
+
+    return cfg
+
+
+def store_gateway_config():
+    """Mimir Store Gateway configuration."""
+    cfg = {"sharding_ring": {"replication_factor": 1}}
+
+    return cfg
+
+
+def memberlist_config(nodename, peers):
+    """Mimir Member List configuration.
+
+    Each member of a Mimir cluster needs to set its own "memberlist".
+    Members in this cluster are part of a replica set.
+
+    Args:
+        nodename: a string name for this memberlist node.
+        peers: a list of dictionaries providing the set of members
+            which are part of the current Mimir cluster. Each dictionary
+            in this list has as its values the hostnames of the memberlist.
+            The keys are typical the node names.
+    """
+    cfg = {"node_name": nodename, "join_members": list(peers.values())}
+
+    return cfg
